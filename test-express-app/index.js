38f7fe08974e3c52496d411152dd83d6ab9908fb
Sergey Manshin <vvintpann@gmail.com>
2021-01-20 22:55:41 +0300

    add basic lint config

--- test-express-app/index.js
+++ test-express-app/index.js
@@ -6,8 +6,8 @@ const PORT = 3000;
 const app = express();
 app.use(cors());
 
-app.use('/test', async (req, res) => {
+app.get('/test', async (req, res) => {
   res.status(200).send('success\n');
-})
+});
 
-app.listen(PORT,() => console.log('Test app is listening'));
+app.listen(PORT, () => console.log('Test app is listening'));
